2a504de402fd0c3839eda738e7b6394e5311f265
DINESH THIRU <dineshthiru36@gmail.com>
2023-10-28 02:48:51 +0530

    Updated

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import router from "./Routes/index.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import morgan from "morgan";
 
 const app = express();
+dotenv.config();
+app.use(morgan('dev'))
 
 // app.use((req, res, next) => {
 //   console.log("hi from middleware use")
@@ -12,5 +17,7 @@ app.get("/", function (req, res) {
 })
 
 app.use("/api/v1", router)
+
+mongoose.connect(process.env.MONGOURL).then(() => console.log("Database connected"))
 
 app.listen(8000)
