2a91eaffff9e2beaa7e6d1abd313b32721dc0293
DINESH THIRU <dineshthiru36@gmail.com>
2023-10-30 19:19:54 +0530

    Updated

--- Controllers/Auth.controllers.js
+++ Controllers/Auth.controllers.js
@@ -1,17 +1,42 @@
 import UserModal from "../Modals/User.modal.js";
+import bcrypt from "bcrypt"
+import Jwt from "jsonwebtoken";
 
-export const Login = (req, res) => {
-  res.send("Login pg")
+export const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) return res.status(401).json({ success: false, message: "all All fields are mandatory" })
+
+    const user = await UserModal.findOne({ email: email })
+
+    if (!user) return res.status(401).json({ success: false, message: "Email is wrong or not registered" })
+    // console.log(user, "user")
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+    // console.log(isPasswordCorrect, "passsss")
+
+    if (!isPasswordCorrect) return res.status(401).json({ success: false, message: "Your password is incorrect" })
+
+    const token = await Jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+    console.log.apply(token,"token")
+
+    return res.status(200).json({ success: true, message: "Login successfull", user: { name: user.name, id: user._id } })
+
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error })
+  }
 }
 
 export const Register = async (req, res) => {
   try {
-
     const { name, email, password, phone } = req.body;
 
     if (!name || !email || !password || !phone) return res.status(401).json({ success: false, message: "All fields are mandatory" })
 
-    const user = new UserModal({ name, email, password, phone })
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = new UserModal({ name, email, password: hashedPassword, phone })
 
     await user.save();
 
@@ -20,5 +45,20 @@ export const Register = async (req, res) => {
   } catch (error) {
     return res.status(500).json({ success: false, message: error })
   }
+}
 
+export const getCurrentUser = async (req, res) => {
+  try {
+    const { id } = req.body;
+    if (!id) return res.status(401).json({ success: false, message: "ID is required" })
+
+    const user = await UserModal.findById(id);
+
+    if (!user) return res.status(401).json({ success: false, message: "User not found" })
+
+    return res.status(200).json({ success: true, user: { name: user.name, id: user._id } })
+
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error })
+  }
 }
