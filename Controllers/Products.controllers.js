a8bae7836d710e3fc83255ad2710b144c111b562
DINESH THIRU <dineshthiru36@gmail.com>
2023-11-02 23:42:51 +0530

    Updated

--- Controllers/Products.controllers.js
+++ Controllers/Products.controllers.js
@@ -5,3 +5,7 @@ export const getAllProducts = (req, res) => {
 export const getSingleProduct = (req, res) => {
   res.send("Single products")
 }
+
+export const addProduct = (req, res) => {
+  res.send("Added a products")
+}
