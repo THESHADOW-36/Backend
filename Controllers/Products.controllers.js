7506c8bec509c27544c1e9a5f93adb5171a95744
DINESH THIRU <dineshthiru36@gmail.com>
2023-11-04 03:15:24 +0530

    Updated

--- Controllers/Products.controllers.js
+++ Controllers/Products.controllers.js
@@ -1,11 +1,31 @@
+import ProductModal from "../Modals/Product.modal.js"
+
+
 export const getAllProducts = (req, res) => {
-  res.send("All products")
+  return res.status(200).send("All products")
 }
 
 export const getSingleProduct = (req, res) => {
-  res.send("Single products")
+  return res.status(200).send("Single product")
 }
 
-export const addProduct = (req, res) => {
-  res.send("Added a products")
+export const addProduct = async (req, res) => {
+  // try {
+  //   const { name, price, category, image } = req.body;
+  //   if (!name || !price || !category || !image) return res.status(404).json({ success: false, message: "All fields are required" })
+
+  //   const product = new ProductModal({ name, price, category, image, userId: id })
+  //   console.log("product-", product)
+
+  //   const ress = await product.save();
+  //   console.log("res-", ress)
+
+  //   return res.status(201).json({ success: true, message: "Product added successfully" })
+
+  // } catch (error) {
+  //   console.log("Error-", error)
+  //   return res.status(500).json({ success: false, message: error })
+  // }
+  return res.status(200).send("Added a product")
+
 }
--- Middlewares/AllMiddlewares.js
+++ Middlewares/AllMiddlewares.js
@@ -7,9 +7,9 @@ export const checkUserId = async (req, res, next) => {
     if (user) {
       next();
     } else {
-      return res.status(404).json({ success: false, message: "User not found" })
+      return res.status(404).json({ success: false, message: "ID is not found" })
     }
   } catch (error) {
-    return res.status(500).json({ success: false, message: error })
+    return res.status(500).json({ success: false, message: error.message })
   }
 }
