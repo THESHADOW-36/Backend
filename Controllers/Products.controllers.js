ad3ae464744aebaf15cc74b2fdc24816a8f80d4e
DINESH THIRU <dineshthiru36@gmail.com>
2023-11-04 17:44:34 +0530

    Updated

--- Controllers/Products.controllers.js
+++ Controllers/Products.controllers.js
@@ -10,22 +10,20 @@ export const getSingleProduct = (req, res) => {
 }
 
 export const addProduct = async (req, res) => {
-  // try {
-  //   const { name, price, category, image } = req.body;
-  //   if (!name || !price || !category || !image) return res.status(404).json({ success: false, message: "All fields are required" })
+  try {
+    const { name, price, category, image, id} = req.body;
+    if (!name || !price || !category || !image) return res.status(404).json({ success: false, message: "All fields are required" })
 
-  //   const product = new ProductModal({ name, price, category, image, userId: id })
-  //   console.log("product-", product)
+    const product = new ProductModal({ name, price, category, image, userId:id})
+    // console.log("product-", product)
 
-  //   const ress = await product.save();
-  //   console.log("res-", ress)
+    const ress = await product.save();
+    // console.log("res-", ress)
 
-  //   return res.status(201).json({ success: true, message: "Product added successfully" })
+    return res.status(201).json({ success: true, message: "Product added successfully" })
 
-  // } catch (error) {
-  //   console.log("Error-", error)
-  //   return res.status(500).json({ success: false, message: error })
-  // }
-  return res.status(200).send("Added a product")
-
+  } catch (error) {
+    console.log("Error-", error)
+    return res.status(500).json({ success: false, message: error })
+  }
 }
