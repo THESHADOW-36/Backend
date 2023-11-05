8f1aa7bd5b2b45b0d7e0d76d7edddfac7cb3398c
DINESH THIRU <dineshthiru36@gmail.com>
2023-11-05 21:19:05 +0530

    Updated

--- Controllers/Products.controllers.js
+++ Controllers/Products.controllers.js
@@ -1,20 +1,37 @@
 import ProductModal from "../Modals/Product.modal.js"
 
 
-export const getAllProducts = (req, res) => {
-  return res.status(200).send("All products")
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await ProductModal.find({}).limit(10)
+    if (products.length) {
+      return res.status(200).json({ success: true, message: "Products found", product: { products } })
+    }
+    return res.status(404).json({ success: false, message: "Product are unavailable" })
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error })
+  }
 }
 
-export const getSingleProduct = (req, res) => {
-  return res.status(200).send("Single product")
+export const getSingleProduct = async (req, res) => {
+  try {
+    const { productId } = req.body
+    if (!productId) return res.status(404).json({ success: false, message: "Product ID is required" })
+    const product = await ProductModal.findById(productId).select("name -_id price image")
+    if (product) {
+      return res.status(200).json({ success: true, message: "Products found" })
+    }
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error })
+  }
 }
 
 export const addProduct = async (req, res) => {
   try {
-    const { name, price, category, image, id} = req.body;
+    const { name, price, category, image, id } = req.body;
     if (!name || !price || !category || !image) return res.status(404).json({ success: false, message: "All fields are required" })
 
-    const product = new ProductModal({ name, price, category, image, userId:id})
+    const product = new ProductModal({ name, price, category, image, userId: id })
     // console.log("product-", product)
 
     const ress = await product.save();
@@ -24,6 +41,28 @@ export const addProduct = async (req, res) => {
 
   } catch (error) {
     console.log("Error-", error)
+    return res.status(500).json({ success: false, message: error })
+  }
+}
+
+export const filterProducts = async (req, res) => {
+  try {
+    const { skip, page = 10, query, sorting } = req.body;
+
+    const updatedQuery = { category: query }
+    // updatedQuery.category = query
+
+    const name = sorting.replace(/^-/, "")
+
+    const order = sorting[0] == "-" ? "-" : "";
+
+    const updatedSorting = { [name]: `${order}1` };
+
+    const products = await ProductModal.find(updatedQuery).skip(skip * 10).limit(page).sort({ sorting: 1 })
+
+    return res.status(201).json({ success: true, message: "Product found", products: products })
+
+  } catch (error) {
     return res.status(500).json({ success: false, message: error })
   }
 }
--- Routes/Product.routes.js
+++ Routes/Product.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { checkUserId } from "../Middlewares/AllMiddlewares.js";
-import { addProduct } from "../Controllers/Products.controllers.js";
+import { addProduct, filterProducts, getAllProducts, getSingleProduct } from "../Controllers/Products.controllers.js";
 
 const router = Router()
-
+router.post("/filter-products",filterProducts)
 router.post("/add-product", checkUserId, addProduct)
+router.post("/get-single-product", getSingleProduct)
+router.get("/get-all-product", getAllProducts)
 
 export default router
