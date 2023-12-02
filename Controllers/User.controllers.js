a16e8593061b45f32c2f419bcbbec10019950857
DINESH THIRU <dineshthiru36@gmail.com>
2023-12-02 13:23:47 +0530

    My-Store

--- Controllers/User.controllers.js
+++ Controllers/User.controllers.js
@@ -1,4 +1,5 @@
 import UserModal from "../Modals/User.modal.js";
+import ProductModal from "../Modals/Product.modal.js";
 
 export const addToCart = async (req, res) => {
   try {
@@ -21,6 +22,39 @@ export const cart = async (req, res) => {
 
     const user = await UserModal.findById(id)
     if (!user) return res.status(404).json({ success: false, message: "User not found" })
+
+    if (user) {
+      var userCart = [];
+      for (var i = 0; i < user.cart.length; i++) {
+        const productData = await ProductModal.findById(user.cart[i])
+        userCart.push(productData)
+      }
+      console.log(userCart, "userCart")
+      return res.status(201).json({ success: true, message: "Products fetched successfully.", products: userCart })
+    }
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error })
+  }
+}
+
+export const deleteToCart = async (req, res) => {
+  try {
+    const { proId, userId } = req.body;
+    if (!proId || userId) return res.status(404).json({ success: false, message: "User and products are mandatory" })
+
+    const user = await UserModal.findById(userId)
+    if (!user) return res.status(404).json({ success: false, message: "User not found" })
+
+    const index = user.cart.indexOf(proId)
+    user.cart.splice(index, 1)
+    await user.save();
+
+    var userCart = [];
+    for (var i = 0; i < user.cart.length; i++) {
+      const productData = await ProductModal.findById(user.cart[i])
+      userCart.push(productData)
+    }
+    return res.status(201).json({ success: true, message: "Product deleted successfully.", products: userCart })
   } catch (error) {
     return res.status(500).json({ success: false, message: error })
   }
--- Routes/User.routes.js
+++ Routes/User.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { addToCart, cart } from "../Controllers/User.controllers.js";
+import { addToCart, cart, deleteToCart } from "../Controllers/User.controllers.js";
 
 const router = Router()
 
 router.post("/add-cart", addToCart)
-// router.post("/delete-cart", deleteToCart)
+router.post("/delete-cart", deleteToCart)
 router.post("/cart", cart)
 
 export default router;
